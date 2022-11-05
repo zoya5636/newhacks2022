a74bb1cd8c0bfeffda9519304558316dd3730aac
zoya5636 <83867635+zoya5636@users.noreply.github.com>
2022-11-05 14:36:52 -0400

    Sends SMS and Opens on localhost:8000
    - Can send messages on SMS
    - Listens to port 8000

---
+++ phone_test.js
@@ -0,0 +1,41 @@
+const Vonage = require('@vonage/server-sdk')
+
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
+
+let http = require('http');
+
+//app.use(express.static('directory'))
+
+const vonage = new Vonage({
+  apiKey: "b2406ad6",
+  apiSecret: "Dfyp1Lji55NI75Zy"
+})
+
+const from = "15815347282"
+const to = "ADD-PHONE-NUMBER"
+const text = 'A text message sent using the Vonage SMS API'
+
+vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+        console.log(err);
+    } else {
+        if(responseData.messages[0]['status'] === "0") {
+            console.log("Message sent successfully.");
+        } else {
+            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+        }
+    }
+})
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.write('Hi There!');
+    response.end();
+};
+
+http.createServer(handleRequest).listen(8000);
